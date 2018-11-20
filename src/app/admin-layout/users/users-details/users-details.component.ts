d4e08f2ac6169a9d90d4914241a86a3c8f111ea1
sramirez <sramirez@solidgeargroup.com>
2018-11-20 14:07:20 +0100

    09 - Retrieve params form route in users details

--- src/app/admin-layout/users/users-details/users-details.component.ts
+++ src/app/admin-layout/users/users-details/users-details.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-users-details',
-  templateUrl: './users-details.component.html',
-  styleUrls: ['./users-details.component.scss']
+    selector: 'app-users-details',
+    templateUrl: './users-details.component.html',
+    styleUrls: ['./users-details.component.scss']
 })
 export class UsersDetailsComponent implements OnInit {
 
-  constructor() { }
+    private id: Number;
 
-  ngOnInit() {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
+    ngOnInit() {
+        this.route.params
+        // (+) converts string 'id' to a number
+            .subscribe(params => {this.id = +params['id']})
+        this.route.data
+            .subscribe(data => console.log(data))
+    }
 
 }
