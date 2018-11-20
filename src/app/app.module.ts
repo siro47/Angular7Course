175d18746d33b15f9dff3f6b939d73c8e1cf52e5
sramirez <sramirez@solidgeargroup.com>
2018-11-20 13:53:43 +0100

    06 - Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { AppComponent } from './app.component';
 import { CustomCardComponent } from './custom-card/custom-card.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomCardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-      BrowserAnimationsModule,
-      MatIconModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CustomCardComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatButtonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
