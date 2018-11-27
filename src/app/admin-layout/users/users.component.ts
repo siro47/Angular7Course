defeae0beae079e50bab994590396ef114749368
sramirez <sramirez@solidgeargroup.com>
2018-11-27 10:10:16 +0100

    15 - Pipe functions to search form

--- src/app/admin-layout/users/users.component.ts
+++ src/app/admin-layout/users/users.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersService} from "./users.service";
 import {FormControl, FormGroup} from "@angular/forms";
+import {Observable} from "rxjs/internal/Observable";
+
+import {debounce, map} from "rxjs/operators";
+import {interval} from "rxjs/internal/observable/interval";
 
 @Component({
     selector: 'app-users',
@@ -16,16 +20,23 @@ export class UsersComponent implements OnInit {
         private usersService: UsersService
     ) {
         this.searchForm = new FormGroup({
-            search: new FormControl()
+            search: new FormControl('')
         })
     }
 
     ngOnInit() {
-        this.users = this.usersService.getUsers();
+        this.users = this.usersService.getUsers(null);
+
+        const mapParam = map(value => value.search);
+        const waitUntilStop = debounce(() => interval(700));
 
         this.searchForm.valueChanges
+            .pipe(
+                mapParam,
+                waitUntilStop
+            )
             .subscribe(value => {
-                this.users = this.usersService.getUsers(value.search);
+                this.users = this.usersService.getUsers(value);
             })
     }
 
